bbbe8e9fcf51c189a785016d4a9170fdf3c3f8f4
muzsay22 <77saymuz@stud.akademiet.no>
2021-09-27 11:17:01 +0200

    ja

---
+++ app/src/main/java/com/example/quizapp/validator.kt
@@ -0,0 +1,4 @@
+package com.example.quizapp
+
+object Validator {
+ fun validateInput(amount: Int, desc: String){}
---
+++ app/src/test/java/com/example/quizapp/ValidatorTest.kt
@@ -0,0 +1,14 @@
+package com.example.quizapp
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ValidatorTest
+
+
+
+@Test
+fun whenInputIsValid(){
